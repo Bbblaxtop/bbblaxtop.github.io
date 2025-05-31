a6d5c6b4833a1375a6d01cb5492cd7083d73fe86
salat20 <kormirik333@gmail.com>
2025-05-31 18:17:17 +0500

    global update & nickname change

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkPass(){
+    if (document.getElementById("code").value.toLowerCase() == "test"){
+        alert("This is a test code!")
+    } else {
+        alert("Code does not exist!")
+    }
+}
--- turtleRNG/scripts/script.js
+++ turtleRNG/scripts/script.js
@@ -4,3 +4,38 @@ tg.expand()
 tg.setHeaderColor("#001122")
 tg.setBackgroundColor("#001122")
 tg.setBottomBarColor("#001122")
+
+var turtles = [
+    {
+        title: "Classic Turtle",
+        rarity: 0,
+        pic: "assets/turtles/0.png"
+    },
+    {
+        title: "Rich Turtle",
+        rarity: 6,
+        pic: "assets/turtles/0.png"
+    }
+]
+
+var rarities = ["#00ff00","#222222","#00aaff","#aa00ff","#cc00ff","#aa0000","#FFD700"]
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function roll(){
+    var result = Math.round(getRandomArbitrary(0,turtles.length))
+
+    document.getElementById("img-t").src = turtles[result].pic
+    document.getElementById("image-turtle").style.background = "linear-gradient(0deg, " + rarities[turtles[result].rarity] + ",rgb(0,0,0,0)) no-repeat"
+    document.getElementById("img-t").style.filter = "brightness(1)"
+}
+
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}

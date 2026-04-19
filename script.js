9cce4e5ee84ab8639e7b3e7c999fe5c62e718046
gridblue <150575974+Bbblaxtop@users.noreply.github.com>
2026-04-19 16:12:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function checkPass(){
     if (document.getElementById("code").value.toLowerCase() == "test"){
         alert("This is a test code!")
+    } else if (document.getElementById("code").value.toLowerCase() == "monkey4clipping") {
+        location.href = "/monkey4clipping_quiz"
     } else {
         alert("Code does not exist!")
     }

